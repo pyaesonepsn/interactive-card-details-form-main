88d7086eb3dfd8c39cab398189dcc74f44888eb8
pyaesonepsn <pyaesonepsn@gmail.com>
2022-08-25 00:51:42 +0630

    changes made

--- main.js
+++ main.js
@@ -1,66 +1,40 @@
-const success = document.querySelector(".completed-state");
-const form = document.querySelector("#form");
-const inputs = document.querySelectorAll('.form-input');
-const nameInput = document.querySelector('#name');
-const numberInput = document.querySelector('#number');
-const monthInput = document.querySelector('#month');
-const yearInput = document.querySelector('#year');
-const cvcInput = document.querySelector('#cvc');
-const continueBtn = document.querySelector('.confirmBtn');
-const isEmptyErrorMessage = "Can't be blank";
-const hasCharactersErrorMessage = "Wrong format,numbers only";
-const card_number = document.querySelector('.left-card-number');
-const card_name = document.querySelector('.name');
-const card_month = document.querySelector('.month');
-const card_year = document.querySelector('.year');
-const card_cvc = document.querySelector('.card-cvc');
+let success = document.querySelector(".success");
+let form = document.querySelector(".form");
+let inputs = document.querySelectorAll(".form__input");
+let nameInput = document.querySelector("#name");
+let numberInput = document.querySelector("#number");
+let monthInput = document.querySelector("#month");
+let yearInput = document.querySelector("#year");
+let cvcInput = document.querySelector("#cvc");
+let button_continue = document.querySelector(".button-continue");
+let isEmptyErrorMessage = "Can't be blank";
+let hasCharactersErrorMessage = "Wrong format, numbers only";
+let card_number = document.querySelector(".card-number");
+let card_name = document.querySelector(".name");
+let card_month = document.querySelector(".month");
+let card_year = document.querySelector(".year");
+let card_cvc = document.querySelector(".card-cvc");
 
-let removeErrorStyle = (input) => {
-    input.classList.remove('error');
-}
-
-let removeErrorMessage = (input) => {
-    let error = document.getElementById(input.dataset.target);
-    error.classList.remove('visible');
-}
-
-let isEmpty = (value) => {
-    return !value.length;
-}
-
-let hasCharacters = (value) => {
-    return !/^\d+$/.test(value);
-}
-
-let displayError = (input,message) => {
-    let error = document.getElementById(input.dataset.target);
-    error.textContent = message;
-    error.classList.add('visible');
-    input.classList.remove('error');
-}
-
-nameInput.addEventListener('keyup', () => {
+nameInput.addEventListener("keyup", () => {
     card_name.textContent = nameInput.value;
-    if(isEmpty(nameInput.value)){
+    if (isEmpty(nameInput.value)) {
         displayError(nameInput, isEmptyErrorMessage);
         return;
     }
     removeErrorStyle(nameInput);
     removeErrorMessage(nameInput);
-})
+});
 
 let formatCreditCardNumber = (str) => {
-    return str.match(/.{1,4}/g).join(' ');
-}
-
-numberInput.addEventListener('keypress',(event) => {
+    return str.match(/.{1,4}/g).join(" ");
+};
+numberInput.addEventListener("keypress", (event) => {
     event.preventDefault();
-    let value = numberInput.value.split(' ').join('');
-    if(value.length === 16) return;
-    let newValue = numberInput.value.split(' ').join('') + event.key;
+    let value = numberInput.value.split(" ").join("");
+    if (value.length === 16) return;
+    let newValue = numberInput.value.split(" ").join("") + event.key;
     numberInput.value = formatCreditCardNumber(newValue);
-})
-
+});
 numberInput.addEventListener("keyup", () => {
     card_number.textContent = numberInput.value;
     let value = number.value.split(" ").join("");
@@ -78,7 +52,6 @@ numberInput.addEventListener("keyup", () => {
     removeErrorStyle(numberInput);
     removeErrorMessage(numberInput);
 });
-
 monthInput.addEventListener("keyup", () => {
     card_month.textContent = monthInput.value;
     if (isEmpty(monthInput.value)) {
@@ -92,7 +65,6 @@ monthInput.addEventListener("keyup", () => {
     removeErrorStyle(monthInput);
     removeErrorMessage(monthInput);
 });
-
 yearInput.addEventListener("keyup", () => {
     card_year.textContent = yearInput.value;
     if (isEmpty(yearInput.value)) {
@@ -106,7 +78,6 @@ yearInput.addEventListener("keyup", () => {
     removeErrorStyle(yearInput);
     removeErrorMessage(yearInput);
 });
-
 cvcInput.addEventListener("keyup", () => {
     card_cvc.textContent = cvcInput.value;
     if (isEmpty(cvcInput.value)) {
@@ -120,6 +91,29 @@ cvcInput.addEventListener("keyup", () => {
     removeErrorStyle(cvcInput);
     removeErrorMessage(cvcInput);
 });
+let displayError = (input, message) => {
+    let error = document.getElementById(input.dataset.target);
+    error.textContent = message;
+    error.classList.add("visible");
+    input.classList.add("error");
+};
+
+let removeErrorStyle = (input) => {
+    input.classList.remove("error");
+};
+
+let removeErrorMessage = (input) => {
+    let error = document.getElementById(input.dataset.target);
+    error.classList.remove("visible");
+};
+
+let isEmpty = (value) => {
+    return !value.length;
+};
+
+let hasCharacters = (value) => {
+    return !/^\d+$/.test(value);
+};
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -158,9 +152,8 @@ const reset = () => {
     card_cvc.textContent = "000";
 };
 
-continueBtn.addEventListener("click", () => {
+button_continue.addEventListener("click", () => {
     reset();
     form.style.display = "flex";
     success.style.display = "none";
 });
-
